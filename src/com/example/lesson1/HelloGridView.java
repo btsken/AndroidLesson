3c3776bb69a888cbfac31ff4dc3b2fc3aa9f0885
Ken Huang <btsken@gmail.com>
2013-12-17 23:21:57 +0800

    gallery add imageLoader

--- src/com/example/lesson1/HelloGridView.java
+++ src/com/example/lesson1/HelloGridView.java
@@ -1,5 +1,10 @@
 package com.example.lesson1;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.*;
@@ -7,13 +12,22 @@ import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class HelloGridView extends Activity {
-	/** Called when the activity is first created. */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gridview);
+
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+				getApplicationContext())
+				.threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())
+				.tasksProcessingOrder(QueueProcessingType.LIFO).build();
+		ImageLoader.getInstance().init(config);
+
 		GridView gridview = (GridView) findViewById(R.id.gridview);
-		gridview.setAdapter(new ImageAdapter(this));
+		gridview.setAdapter(new ImageAdapter(this, ImageUrl.IMAGES));
 
 		gridview.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View v,
